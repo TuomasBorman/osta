26c390651fade26c6d7e13236f31589fe9a32d2c
TuomasBorman <tvborm@utu.fi>
2023-01-08 01:55:36 +0200

    up

--- src/ostan/clean_data.py
+++ src/ostan/clean_data.py
@@ -130,3 +130,78 @@ def __clean_sums(df):
                                                            "price_ex_vat"]):
             df["vat_amount"] = df["total"] - df["price_ex_vat"]
     return df
+
+
+def __standardize_dates(df, **args):
+    """
+    This function converts dates into same format.
+    Input: df
+    Output: df
+    """
+    # Get date column
+    col = df.loc[:, "date"]
+    # Get date format
+    date_format = __get_date_format(col, **args)
+    day_first = date_format[0]
+    year_first = date_format[1]
+
+
+def __get_date_format(df, dayfirst=True, yearfirst=False, **args):
+    """
+    This function identifies the format of dates.
+    Input: df
+    Output: dayfirst and yearfirst arguments that can be
+    passed into dateutils.parser
+    """
+    # Split dates from separator. Result is multiple columns
+    # with year, month and day separated
+    df = df.astype(str).str.split(r"[-/.]", expand=True)
+    # If the split was succesful
+    if df.shape[1] > 1:
+        # Remove columns that did have problems / did not split
+        df = df.dropna().copy()
+        # Convert columns to integer if possible
+        for col in df.columns:
+            if all(df.loc[:, col].astype(str).str.isdigit()):
+                df.loc[:, col] = df.loc[:, col].astype(int)
+        # Get years, months, and days
+        year = list(range(1970, 2050))
+        month = list(range(1, 13))
+        day = list(range(1, 32))
+        # Get those values that distinguish days and years
+        year = list(set(year).difference(day))
+        day = list(set(day).difference(month))
+        # Check if these values can be found from the data
+        data = {
+            "any_in_day": list(any(df[col].isin(day)) for col in df.columns),
+            "any_in_year": list(any(df[col].isin(year)) for col in df.columns)
+            }
+        df_res = pd.DataFrame(data)
+        # Initialize result list, loop over columns that have years,
+        # months and days
+        result = []
+        for i, col in enumerate(df.columns):
+            # Get the result of specific column
+            temp = df_res.iloc[i, :]
+            # Check if it is year
+            if temp["any_in_year"]:
+                result.append("year")
+            # Check if it is day
+            elif temp["any_in_day"]:
+                result.append("day")
+            else:
+                result.append("month")
+        # If result does not include all year, month and day, use
+        # default settings
+        if all(i in result for i in ["year", "month", "day"]):
+            # If year is first
+            yearfirst = True if result.index("year") == 0 else False
+            # If day comes before month
+            if (yearfirst and
+                result.index("day") == 1) or (not yearfirst and
+                                              result.index("day") == 2):
+                dayfirst = True
+            else:
+                dayfirst = False
+    res = [dayfirst, yearfirst]
+    return res
