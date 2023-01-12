839b3682694899041f34565aee7cf26db30033f2
TuomasBorman <tvborm@utu.fi>
2023-01-12 14:49:24 +0200

    up

--- data/get_service_codes.py
+++ data/get_service_codes.py
@@ -7,70 +7,65 @@ path = ("https://vkazprodwordpressstacc01.blob.core.windows.net" +
         "/wordpress/2020/04/Liite-5.-Kunnan-ja-kuntayhtyman-" +
         "palveluluokitus-Excel-14.12.2022.xlsx")
 
+
+def read_service_code(path, sheet_name, year, column_names=None):
+    # Read file
+    df = pd.read_excel(path, sheet_name=sheet_name)
+    # Drop empty rows and columns
+    df = df.dropna(axis=0, thresh=df.shape[1]-2)
+    df = df.dropna(axis=1, thresh=df.shape[1]-2)
+    # If first rows were additional information, adjust column names
+    if all(df.index != 0):
+        df.columns = df.iloc[0, :]
+        df.drop([df.index[0]], inplace=True)
+    # Reset index
+    df.reset_index(inplace=True, drop=True)
+    # Add year
+    df["service_cat_year"] = year
+    # Change columns
+    if column_names is not None:
+        df.columns = column_names
+    return df
+
+
 # 2021 service category
-df_2021 = pd.read_excel(path, sheet_name="Palveluluokitus 2021")
-# Drop empty rows and columns
-df_2021 = df_2021.dropna(axis=0, thresh=df_2021.shape[1]-2)
-df_2021 = df_2021.dropna(axis=1, thresh=df_2021.shape[1]-2)
-# If first rows were additional information, adjust column names
-if all(df_2021.index != 0):
-    df_2021.columns = df_2021.iloc[0, :]
-    df_2021.drop([df_2021.index[0]], inplace=True)
-df_2021.reset_index(inplace=True, drop=True)
-
-# 2022 service category
-df_2022 = pd.read_excel(path, sheet_name="Palveluluokitus 2022")
-# Drop empty rows and columns
-df_2022 = df_2022.dropna(axis=0, thresh=df_2021.shape[1]-2)
-df_2022 = df_2022.dropna(axis=1, thresh=df_2021.shape[1]-2)
-# If first rows were additional information, adjust column names
-if all(df_2022.index != 0):
-    df_2022.columns = df_2022.iloc[0, :]
-    df_2022.drop([df_2022.index[0]], inplace=True)
-df_2022.reset_index(inplace=True, drop=True)
-
-# 2023 service category
-df_2023 = pd.read_excel(path, sheet_name="Palveluluokitus 2022")
-# Drop empty rows and columns
-df_2023 = df_2023.dropna(axis=0, thresh=df_2021.shape[1]-2)
-df_2023 = df_2023.dropna(axis=1, thresh=df_2021.shape[1]-2)
-# If first rows were additional information, adjust column names
-if all(df_2023.index != 0):
-    df_2023.columns = df_2023.iloc[0, :]
-    df_2023.drop([df_2023.index[0]], inplace=True)
-df_2023.reset_index(inplace=True, drop=True)
-
-# Change column names
-df_2021.columns = [
+cols_2021 = [
     "service_cat_name_high",
     "service_cat",
     "service_cat_name",
     "service_cat_name_short",
     "service_desc",
     "service_info",
-    "service_info_ref"
+    "service_info2",
+    "service_cat_year"
     ]
-df_2022.columns = [
-    "service_cat_name_high",
-    "service_cat",
-    "service_cat_name",
-    "service_desc",
-    "service_info",
-    "service_info_changes"
-    ]
-df_2023.columns = [
-    "service_cat_name_high",
-    "service_cat",
-    "service_cat_name",
-    "service_desc",
-    "service_info",
-    "service_info_changes"
-    ]
+df_2021 = read_service_code(path, sheet_name="Palveluluokitus 2021",
+                            column_names=cols_2021, year=2021)
 
-# Add year
-df_2021["service_cat_year"] = 2021
-df_2022["service_cat_year"] = 2022
-df_2023["service_cat_year"] = 2023
+# 2022 service category
+cols_2022 = [
+    "service_cat_name_high",
+    "service_cat",
+    "service_cat_name",
+    "service_desc",
+    "service_info",
+    "service_info2",
+    "service_cat_year"
+    ]
+df_2022 = read_service_code(path, sheet_name="Palveluluokitus 2022",
+                            column_names=cols_2022, year=2022)
+
+# 2023 service category
+cols_2023 = [
+    "service_cat",
+    "service_cat_name",
+    "service_desc",
+    "service_info",
+    "service_info2",
+    "service_cat_year"
+    ]
+df_2023 = read_service_code(path, sheet_name="Palveluluokitus 2023",
+                            column_names=cols_2023, year=2023)
 
 # Combine all DFs
 df = pd.concat([df_2021, df_2022, df_2023])
