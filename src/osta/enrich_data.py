2c581e50f28f37e93be87fbe8864a4e09b59d9f5
TuomasBorman <tvborm@utu.fi>
2023-01-23 21:31:46 +0200

    up

---
+++ src/osta/enrich_data.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import osta.__utils as utils
+import pandas as pd
+import warnings
+import pkg_resources
+
+
+def enrich_data(df, **args):
+    """
+    Change column names of pandas.DataFrame
+
+    Arguments:
+        ```
+        df: pandas.DataFrame containing invoice data.
+
+        ```
+
+    Details:
+
+    Examples:
+        ```
+
+        ```
+
+    Output:
+
+
+    """
+    # INPUT CHECK
+    # df must be pandas DataFrame
+    if not utils.__is_non_empty_df(df):
+        raise Exception(
+            "'df' must be non-empty pandas.DataFrame."
+            )
