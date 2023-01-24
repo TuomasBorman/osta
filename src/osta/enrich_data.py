496d2e57f5e15de7cd30c9e3811662bb1e667d49
TuomasBorman <tvborm@utu.fi>
2023-01-24 22:34:51 +0200

    up

--- src/osta/enrich_data.py
+++ src/osta/enrich_data.py
@@ -40,12 +40,11 @@ def enrich_data(df, **args):
     return df
 
 
-def __add_org_data(df, disable_org=False, org_data=None, **args):
+def __add_org_data(df, disable_org=False, org_data=None):
     """
-    This function prepares organization data to be checked, and calls
-    function that checks it.
-    Input: df
-    Output: df with standardized organization data
+    This function adds organization data to dataset.
+    Input: df (and dataset to be added)
+    Output: enriched df
     """
     # INPUT CHECK
     if not (utils.__is_non_empty_df(org_data) or org_data is None):
@@ -62,27 +61,154 @@ def __add_org_data(df, disable_org=False, org_data=None, **args):
     if disable_org or len(cols_to_check) == 0:
         return df
     # INPUT CHECK END
-    # Column of db that are matched with columns that are being checked
-    # Subset to match with cols_to_check
-    cols_to_match = ["bid", "vat_number", "number", "name"]
-    cols_to_match = [cols_to_match[i] for i, x in enumerate(cols_df)
-                     if x in cols_to_check]
     # Load default database
     if org_data is None:
         # path = pkg_resources.resource_filename(
         #     "osta", "resources/" + "municipality_codes.csv")
         path = "~/Python/osta/src/osta/resources/" + "municipality_codes.csv"
         org_data = pd.read_csv(path, index_col=0)
+    # Column of db that are matched with columns that are being checked
+    # Subset to match with cols_to_check
+    cols_to_match = ["bid", "vat_number", "number", "name"]
+    cols_to_match = [cols_to_match[i] for i, x in enumerate(cols_df)
+                     if x in cols_to_check]
     # Add data from database
     df = __add_data_from_db(df=df, df_db=org_data,
                             cols_to_check=cols_to_check,
                             cols_to_match=cols_to_match,
-                            prefix="org_")
+                            prefix="org")
+    return df
+
+
+def __add_account_data(df, disable_account=False, account_data=None,
+                       subset_account_data=None, **args):
+    """
+    This function adds account data to dataset.
+    Input: df (and dataset to be added)
+    Output: enriched df
+    """
+    # INPUT CHECK
+    if not (utils.__is_non_empty_df(account_data) or account_data is None):
+        raise Exception(
+            "'account_data' must be non-empty pandas.DataFrame or None."
+            )
+    if not isinstance(disable_account, bool):
+        raise Exception(
+            "'disable_account' must be True or False."
+            )
+    # Check if column(s) is found as non-duplicated
+    cols_df = ["account_number", "account_name"]
+    cols_to_check = utils.__not_duplicated_columns_found(df, cols_df)
+    if disable_account or len(cols_to_check) == 0:
+        return df
+    # INPUT CHECK END
+    # Load default database
+    if account_data is None:
+        # path = pkg_resources.resource_filename(
+        #     "osta", "resources/" + "account_info.csv")
+        path = "~/Python/osta/src/osta/resources/" + "account_info.csv"
+        service_data = pd.read_csv(path, index_col=0)
+        # Subset by taking only specific years
+        service_data = __subset_data_based_on_year(df, df_db=account_data,
+                                                   **args)
+        # If user specified balance sheet or income statement,
+        # get only specified accounts
+        if subset_account_data is not None:
+            account_data = account_data.loc[
+                :, account_data["cat_1"] == subset_account_data]
+    # Column of db that are matched with columns that are being checked
+    # Subset to match with cols_to_check
+    cols_to_match = ["number", "name"]
+    cols_to_match = [cols_to_match[i] for i, x in enumerate(cols_df)
+                     if x in cols_to_check]
+    # Add data from database
+    df = __add_data_from_db(df=df, df_db=service_data,
+                            cols_to_check=cols_to_check,
+                            cols_to_match=cols_to_match,
+                            prefix="service")
+    return df
+
+
+def __add_service_data(df, disable_service=False, service_data=None, **args):
+    """
+    This function adds service data to dataset.
+    Input: df (and dataset to be added)
+    Output: enriched df
+    """
+    # INPUT CHECK
+    if not (utils.__is_non_empty_df(service_data) or service_data is None):
+        raise Exception(
+            "'org_data' must be non-empty pandas.DataFrame or None."
+            )
+    if not isinstance(disable_service, bool):
+        raise Exception(
+            "'disable_service' must be True or False."
+            )
+    # Check if column(s) is found as non-duplicated
+    cols_df = ["service_cat", "service_cat_name"]
+    cols_to_check = utils.__not_duplicated_columns_found(df, cols_df)
+    if disable_service or len(cols_to_check) == 0:
+        return df
+    # INPUT CHECK END
+    # Load default database
+    if service_data is None:
+        # path = pkg_resources.resource_filename(
+        #     "osta", "resources/" + "service_codes.csv")
+        path = "~/Python/osta/src/osta/resources/" + "service_codes.csv"
+        service_data = pd.read_csv(path, index_col=0)
+        # Subset by taking only specific years SIIRRÄ UTILSIIN
+        service_data = __subset_data_based_on_year(df, df_db=service_data,
+                                                   **args)
+    # Column of db that are matched with columns that are being checked
+    # Subset to match with cols_to_check
+    cols_to_match = ["number", "name"]
+    cols_to_match = [cols_to_match[i] for i, x in enumerate(cols_df)
+                     if x in cols_to_check]
+    # Add data from database
+    df = __add_data_from_db(df=df, df_db=service_data,
+                            cols_to_check=cols_to_check,
+                            cols_to_match=cols_to_match,
+                            prefix="service")
+    return df
+
+
+def __add_suppl_data(df, disable_suppl=False, suppl_data=None):
+    """
+    This function adds supplier data to dataset.
+    Input: df and dataset to be added
+    Output: enriched df
+    """
+    # INPUT CHECK
+    if not (utils.__is_non_empty_df(suppl_data) or suppl_data is None):
+        raise Exception(
+            "'org_data' must be non-empty pandas.DataFrame or None."
+            )
+    if not isinstance(disable_suppl, bool):
+        raise Exception(
+            "'disable_suppl' must be True or False."
+            )
+    # Check if column(s) is found as non-duplicated
+    cols_df = ["suppl_id", "suppl_vat_number", "suppl_number", "suppl_name"]
+    cols_to_check = utils.__not_duplicated_columns_found(df, cols_df)
+    if disable_suppl or len(cols_to_check) == 0:
+        return df
+    # INPUT CHECK END
+    if suppl_data is not None:
+        # Column of db that are matched with columns that are being checked
+        # Subset to match with cols_to_check
+        cols_to_match = ["bid", "vat_number", "number", "name", "country"]
+        cols_to_match = [cols_to_match[i] for i, x in enumerate(cols_df)
+                         if x in cols_to_check]
+        # Add data from database
+        df = __add_data_from_db(df=df, df_db=suppl_data,
+                                cols_to_check=cols_to_check,
+                                cols_to_match=cols_to_match,
+                                prefix="suppl")
     return df
 
 
 def __add_data_from_db(df, df_db, cols_to_check, cols_to_match, prefix):
-    # Which column are found from df and df_db
+    # Which column are found from df and df_db SIIRRÄ UTILSIIN
     cols_df = [x for x in cols_to_check if x in df.columns]
     cols_df_db = [x for x in cols_to_match if x in df_db.columns]
     # Drop those columns that do not have match in other df
@@ -92,11 +218,21 @@ def __add_data_from_db(df, df_db, cols_to_check, cols_to_match, prefix):
     else:
         cols_to_match = [cols_df_db[cols_to_check.index(x)] for x in cols_df]
         cols_to_check = cols_df
+    # If identification coluns were not found
+    if len(cols_to_check) == 0 or len(cols_to_match) == 0:
+        warnings.warn(
+            message=f"'{prefix}_data' should include at least one of the "
+            "following columns: 'name' (name), 'number' "
+            "(number), and 'bid' (business ID for organization and "
+            f"supplier data).",
+            category=Warning
+            )
+        return df
+    # Get columns that will be added to data/that are not yet included
+    cols_to_add = [x for x in df_db.columns if x not in cols_to_match]
     # Get only the first variable
     col_to_check = cols_to_check[0]
     col_to_match = cols_to_match[0]
-    # Get columns that will be added to data/that are not yet included
-    cols_to_add = [x for x in df_db.columns if x not in cols_to_match]
     # If there are columns to add
     if len(cols_to_add) > 0:
         # Create temporary columns which are used to merge data
@@ -104,12 +240,16 @@ def __add_data_from_db(df, df_db, cols_to_check, cols_to_match, prefix):
         temp_y = df_db.loc[:, col_to_match]
         # Subset database and add prefix tp column names
         df_db = df_db.loc[:, cols_to_add]
-        df_db.columns = prefix + df_db.columns
-        # If variables can be converted into numeric, do so
-        if (all(temp_x.astype(str).str.isnumeric()) and all(
-                temp_y.astype(str).str.isnumeric())):
+        df_db.columns = prefix + "_" + df_db.columns
+        # If variables can be converted into numeric, do so.
+        # Otherwise convert to object if datatypes are not equal
+        if (all(temp_x.dropna().astype(str).str.isnumeric()) and all(
+                temp_y.dropna().astype(str).str.isnumeric())):
             temp_x = pd.to_numeric(temp_x)
             temp_y = pd.to_numeric(temp_y)
+        elif temp_x.dtype != temp_y.dtype:
+            temp_x = temp_x.astype(str)
+            temp_y = temp_y.astype(str)
         # Add temporary columns to data
         df.loc[:, "temporary_X"] = temp_x
         df_db.loc[:, "temporary_Y"] = temp_y
