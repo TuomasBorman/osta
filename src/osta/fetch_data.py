388f9c6e9e285ab8a54e53536186ab6261ca4b26
TuomasBorman <tvborm@utu.fi>
2023-02-19 22:08:43 +0200

    up

--- src/osta/fetch_data.py
+++ src/osta/fetch_data.py
@@ -7,6 +7,15 @@ import json
 import warnings
 import sys
 import re
+import codecs
+import tempfile
+import urllib.request
+from zipfile import ZipFile
+import shutil
+import os
+import filetype
+import cchardet
+import csv
 
 
 def fetch_data_urls(search_words, **args):
@@ -33,7 +42,6 @@ def fetch_data_urls(search_words, **args):
 
     """
     # INPUT CHECK
-    # df must be pandas DataFrame
     if not isinstance(search_words, str):
         raise Exception(
             "'search_words' must be a string."
@@ -145,4 +153,242 @@ def fetch_data_urls(search_words, **args):
             message="Retrieving URL addresses was not successful.",
             category=Warning
             )
+    return df
+
+
+def read_files(file_path, **args):
+    """
+    Read single CSV or Excel file to pandas.DataFrame.
+
+    Arguments:
+        `file_path`: A string specifying the path or URL address to the file.
+
+    Details:
+        This function reads files to pd.DataFrame.
+
+    Examples:
+        ```
+        df_urls = fetch_data_urls("ostolasku")
+        df_list = read_files(df_urls.loc[1:3, "url"])
+        ```
+
+    Output:
+        pandas.DataFrame or list of pd.DataFrames if file path specifies
+        zipped directory with multiple files.
+
+    """
+    # INPUT CHECK
+    if not (isinstance(file_path, list) or isinstance(file_path, pd.Series)):
+        raise Exception(
+            "'file_path' must be a list of strings."
+            )
+    # INPUT CHECK END
+    # If the file_path contains only one path, create a list from it
+    if isinstance(file_path, str):
+        file_path = [file_path]
+    # For progress bar, specify the width of it
+    progress_bar_width = 50
+    # Loop through file paths
+    df_list = []
+    for i, path_temp in enumerate(file_path):
+        # Update the progress bar
+        percent = 100*((i+1)/len(file_path))
+        sys.stdout.write('\r')
+        sys.stdout.write("Completed: [{:{}}] {:>3}%"
+                         .format('='*int(percent/(100/progress_bar_width)),
+                                 progress_bar_width, int(percent)))
+        sys.stdout.flush()
+        # Try to load, give a warning if not success
+        try:
+            df = read_file(path_temp, **args)
+            # Convert to list if it is not already
+            if not isinstance(df, list):
+                df = [df]
+            # Add to results
+            df_list.extend(df)
+        except Exception:
+            warnings.warn(
+                message=f"Error while loading the following path. "
+                f"It is excluded from the output: \n {path_temp}",
+                category=UserWarning)
+    # Stop progress bar
+    sys.stdout.write("\n")
+    return df_list
+
+
+def read_file(file_path, temp_dir=None, **args):
+    """
+    Read single CSV or Excel file to pandas.DataFrame.
+
+    Arguments:
+        `file_path`: A string specifying the path or URL address to the file.
+
+    Details:
+        This function reads files to pd.DataFrame.
+
+    Examples:
+        ```
+        ```
+
+    Output:
+        pandas.DataFrame or list of pd.DataFrames if file path specifies
+        zipped directory with multiple files.
+
+    """
+    # INPUT CHECK
+    if not isinstance(file_path, str):
+        raise Exception(
+            "'file_path' must be a string."
+            )
+    if not (isinstance(temp_dir, str) or temp_dir is None):
+        raise Exception(
+            "'temp_dir' must be None or string specifying temporary directory."
+            )
+    # INPUT CHECK END
+    # If the file is not located in the local machine
+    if not os.path.exists(file_path):
+        # Store the file_path to url variable
+        url = file_path
+        # If user has not specified temporary directory, get the default
+        if temp_dir is None:
+            # Get the name of higher level tmp directory
+            temp_dir_path = tempfile.gettempdir()
+            temp_dir = temp_dir_path + "/osta_tmp_dir"
+        # Check if spedicified directory exists. If not, create it
+        if not os.path.isdir(temp_dir):
+            os.makedirs(temp_dir)
+        # Get temporary file path
+        file_path = file_path.split("/")
+        file_path = file_path[-1]
+        file_path = os.path.join(temp_dir, file_path)
+        # Try to access the file based on url address if it is not already
+        # laoded in cache
+        if not os.path.exists(file_path):
+            try:
+                urllib.request.urlretrieve(url, file_path)
+            except Exception:
+                raise ValueError(
+                    "'file_name' is not a correct path to local file "
+                    "and it was not possible to retrieve file based "
+                    "on URL address specified by 'file_name'.")
+
+    # Test if the data is in zip format
+    file_type = filetype.guess(file_path)
+    if file_type is not None and file_type.extension == "zip":
+        # Path for extracted files
+        extract_path = os.path.join(tempfile.gettempdir(),
+                                    "invoice_tempfile_ext.tmp")
+        # Remove files from the dir if it already exists
+        if os.path.exists(extract_path):
+            shutil.rmtree(extract_path)
+        # Extract files
+        with ZipFile(file_path, 'r') as zip:
+            zip.extractall(extract_path)
+        # Get all the file paths in extracted folder
+        file_path = []
+        for path, subdirs, files in os.walk(extract_path):
+            for name in files:
+                file_path.append(os.path.join(path, name))
+
+    # If the file_path contains only one path, create a list from it
+    if isinstance(file_path, str):
+        file_path = [file_path]
+
+    # Loop over file paths
+    dfs = []
+    for path_temp in file_path:
+        # Open file as DataFrame
+        df = __detect_format_and_open_file(path_temp, **args)
+        # Store DF tp list
+        dfs.append(df)
+    # If there is only one DF in list, return only the DF
+    if len(dfs) == 1:
+        dfs = dfs[0]
+    return dfs
+
+
+def __detect_format_and_open_file(
+        file_path,
+        encoding=None, guess_encoding=True,
+        delimiter=None, guess_delimiter=True,
+        polish_data=True,
+        **args):
+    """
+    This function is a helper function to load and detect the format of the
+    file that cab be found from the disk and that is not zipped.
+    The returned value is pandas.DataFrame.
+    """
+    # INPUT CHECK
+    if not (isinstance(encoding, str) or encoding is None):
+        raise Exception(
+            "'encoding' must be a string or None."
+            )
+    if not isinstance(guess_encoding, bool):
+        raise Exception(
+            "'guess_encoding' must be a boolean value."
+            )
+    if not (isinstance(delimiter, str) or delimiter is None):
+        raise Exception(
+            "'delimiter' must be a string or None."
+            )
+    if not isinstance(guess_delimiter, bool):
+        raise Exception(
+            "'guess_delimiter' must be a boolean value."
+            )
+    if not isinstance(polish_data, bool):
+        raise Exception(
+            "'polish_data' must be a boolean value."
+            )
+    # INPUT CHECK END
+    # Check if the file is excel file
+    file_type = filetype.guess(file_path)
+    if file_type is not None and (file_type.extension == "xls" or
+                                  file_type.extension == "xlsx"):
+        file_type = "excel"
+    else:
+        file_type = "csv"
+
+    # If encoding is not determined and user wants to guess it for csv file
+    if file_type == "csv" and encoding is None and guess_encoding:
+        # Open the data
+        rawdata = open(file_path, "rb")
+        # Check its encoding
+        encoding = cchardet.detect(rawdata.read())["encoding"]
+
+    # If delimiter is not specified and user wants to guess it for csv file
+    if file_type == "csv" and delimiter is None and guess_delimiter:
+        # Open the data
+        rawdata = codecs.open(file_path, 'r', encoding=encoding)
+        # Get delimiter
+        delimiter = csv.Sniffer().sniff(rawdata.read()).delimiter
+
+    # Based on file format, open the file with correct function
+    if file_type == "excel":
+        df = pd.read_excel(file_path, **args, dtype='unicode')
+    else:
+        df = pd.read_csv(file_path, encoding=encoding, delimiter=delimiter,
+                         **args, dtype='unicode')
+
+    # Polish data, i.e., remove empty rows and columns
+    if polish_data:
+        # Drop empty rows
+        if any(df.isna().sum(axis=1) == df.shape[0]):
+            df.dropna(axis=0, how='all', inplace=True)
+
+        # Drop empty columns
+        if any(df.isna().sum(axis=0) == df.shape[1]):
+            df.dropna(axis=1, how='all', inplace=True)
+
+        # If the first row contained empty values, there are columns named
+        # "Unnamed". Replace column names with the values of the first row
+        if any([True if x.find('Unnamed') != -1 else False
+                for x in df.columns]):
+            df.columns = df.iloc[0, :].values
+            df = df.iloc[1:, :]
+
+        # If there are colmns wih spaces
+        bools = [all(df.iloc[:, x[0]].astype(str).str.isspace())
+                 for x in enumerate(df.columns)]
+        if any(bools):
+            df = df.loc[:, [not x for x in bools]]
     return df
